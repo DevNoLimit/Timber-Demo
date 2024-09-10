9c23001ecfa8228982b7bfc46b2b5b81c97e6056
Mukesh Kumar <rajputmukesh748@gmail.com>
2024-09-10 22:44:37 +0530

    Timber Demo

---
+++ app/src/main/java/com/devnolimit/timberdemo/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.devnolimit.timberdemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.devnolimit.timberdemo.ui.theme.TimberDemoTheme
+import timber.log.Timber
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TimberDemoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Timber.tag("Name").e("Hello World")
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TimberDemoTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/devnolimit/timberdemo/MyApplication.kt
@@ -0,0 +1,16 @@
+package com.devnolimit.timberdemo
+
+import android.app.Application
+import timber.log.Timber
+
+class MyApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+
+}
